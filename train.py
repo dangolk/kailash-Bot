2e59b569da26d6c1eae9be35f5a75a53cd9429c6
sandesh9 <sandesh.prasai@gmail.com>
2021-04-29 18:22:55 -0500

    Initial commit

---
+++ train.py
@@ -0,0 +1,135 @@
+# libraries
+import random
+from keras.optimizers import SGD
+from keras.layers import Dense, Dropout
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+import numpy as np
+import pickle
+import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.python.keras.layers import Flatten
+
+lemmatizer = WordNetLemmatizer()
+nltk.download("punkt")
+nltk.download("wordnet")
+
+
+# init file
+words = []
+classes = []
+documents = []
+ignore_words = ["?", "!"]
+data_file = open("intents.json").read()
+intents = json.loads(data_file)
+
+# words
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
+
+        # take each word and tokenize it
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        # adding documents
+        documents.append((w, intent["tag"]))
+
+        # adding classes to our class list
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+# lemmatizer
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+#words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
+
+print(len(documents), "documents")
+print((documents[0]), "doc0")
+print((documents[1]), "doc1")
+print((documents[2]), "doc2")
+print(len(classes), "classes", classes)
+print(len(words), "unique lemmatized words", words)
+
+pickle.dump(words, open("words.pkl", "wb"))
+pickle.dump(classes, open("classes.pkl", "wb"))
+
+# training initializer
+# initializing training data
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    # initializing bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # lemmatize each word - create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+print("train_x shape ",np.shape(train_x))
+print("train_y shape ", np.shape(train_y))
+print(len(train_x[0]), "train_x[0]")
+print(len(train_x[1]), "train_x[1]")
+print(len(train_x[2]), "train_x[2]")
+print(len(train_x[25]),"train_x[25]")
+print(train_x[0])
+print("Training data created")
+
+# actual training
+# Create model - 5 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.6))
+#model.add(Flatten())
+model.add(Dense(len(train_y[0]), activation="softmax"))
+model.summary()
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+# for choosing an optimal number of training epochs to avoid underfitting or overfitting use an early stopping callback to keras
+# based on either accuracy or loos monitoring. If the loss is being monitored, training comes to halt when there is an 
+# increment observed in loss values. Or, If accuracy is being monitored, training comes to halt when there is decrement observed in accuracy values.
+
+from keras import callbacks
+earlystopping = callbacks.EarlyStopping(monitor ="loss", mode ="auto", patience = 100, restore_best_weights = True)
+
+
+# fitting and saving the model
+history = model.fit(np.array(train_x), np.array(train_y), epochs=1000, batch_size=5, verbose=1,callbacks=earlystopping)
+model.save("chatbot_model.h5", history)
+print("model created")
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
